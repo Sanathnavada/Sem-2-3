1dd88e02df9882af0818d6a7cdf505513be501cf
Sanath Kumar N <96776538+Sanathnavada@users.noreply.github.com>
2021-12-30 00:05:34 +0530

    Add files via upload

---
+++ main (8).c
@@ -0,0 +1,91 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+#include<stdlib.h>
+int eval_post(char postfix[]);
+void push(int tn,int *top,int stack[]);
+int pop(int *top,int stack[]);
+int compute(int op1,int op2,char symbol);
+
+
+
+int main()
+{
+  char postfix[10];
+  int result;
+  printf("enter a postix expression\n");
+  scanf("%s",postfix);
+  printf("the result of evaluvation=%d",eval_post(postfix));
+
+    return 0;
+}
+
+int eval_post(char postfix[])
+{
+    int result;
+    int i=0,top=-1,stack[10],token,op2,op1;
+    char symbol;
+    while(postfix[i]!='\0')
+    {
+         symbol=postfix[i];
+         if(isdigit(symbol))
+          {
+              token=symbol-'0'; 
+              push(token,&top,stack);
+              
+          }
+          else
+           {
+               op2=pop(&top,stack);
+               op1=pop(&top,stack);
+               result=compute(op1,op2,symbol);
+               push(result,&top,stack);
+               
+           }
+          i=i+1;
+    }
+     
+     return (pop(&top,stack));
+     
+}
+
+void push(int tn,int *top,int stack[])
+{
+    ++(*top);
+    stack[(*top)]=tn;
+    
+}
+
+int pop(int *top,int stack[])   
+{
+    return (stack[(*top)--]);
+    
+}
+
+int compute(int op1,int op2,char symbol)
+{
+    switch(symbol)
+    
+    {
+         case '+':return(op1+op2);
+                  break;
+          
+         case '-':return(op1-op2);
+                  break;
+          case'*':return(op1*op2);
+                  break;
+          case '/':return(op1/op2);
+                   break;
+           case'%':return(op1%op2);
+                   break;
+          default:printf("invalid symbol\n");
+          
+    }
+}
+
